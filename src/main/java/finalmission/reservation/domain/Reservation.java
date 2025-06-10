057a98fa4013a203c8ec32ba76f5af2414003c39
황재웅 <wodnd0131@naver.com>
2025-06-10 14:06:28 +0900

    feat: ReservationState 추가

--- src/main/java/finalmission/reservation/domain/Reservation.java
+++ src/main/java/finalmission/reservation/domain/Reservation.java
@@ -3,6 +3,8 @@ package finalmission.reservation.domain;
 import finalmission.member.domain.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,6 +29,10 @@ public class Reservation {
 
     @Column(nullable = false)
     private LocalTime time;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ReservationState state;
 
     @ManyToOne
     @JoinColumn(name = "coach_id")
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO member (name, role)
 VALUES ('브라운', 'COACH'),
        ('몽이', 'CREW');
 
-INSERT INTO reservation (date, time, coach_id, crew_id)
-VALUES ('2030-03-11', '11:00', 1, 2),
-       ('2030-03-11', '11:30', 1, 2);
+INSERT INTO reservation (date, time, state, coach_id, crew_id)
+VALUES ('2030-03-11', '11:00', 'WAITING', 1, 2),
+       ('2030-03-11', '11:30', 'WAITING', 1, 2);
