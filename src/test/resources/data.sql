16cbd3e89d0dc248d2a98b9947815c3a5e2dec9e
황재웅 <wodnd0131@naver.com>
2025-06-10 13:57:41 +0900

    test: test Resources 추가

---
+++ src/test/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO member (name, role)
+VALUES ('브라운', 'COACH'),
+       ('몽이', 'CREW');
+
+INSERT INTO reservation (date, time, coach_id, crew_id)
+VALUES ('2030-03-11', '11:00', 1, 2),
+       ('2030-03-11', '11:30', 1, 2);
